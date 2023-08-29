108413aebc652e01d4e25f1cdca2cf82dcb1aafc
maisma2 <maismail63@gmail.com>
2023-08-29 16:11:48 -0500

    Adding in okhttp3 to start

---
+++ PersonalAPIProject/Main/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/* Creating a program that will convert a word document to a pdf document. First attempt at making a program that
+will rely on a third party api, in this case API2Convert which gives 30 free conversions a day.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
