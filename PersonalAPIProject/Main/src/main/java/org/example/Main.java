492245507976d1425acfb5d821b310db28f32ee7
maisma2 <maismail63@gmail.com>
2023-09-20 15:59:34 -0500

    Photo upload and restart code

--- PersonalAPIProject/Main/src/main/java/org/example/Main.java
+++ PersonalAPIProject/Main/src/main/java/org/example/Main.java
@@ -6,13 +6,15 @@ import com.fasterxml.jackson.*;
 import java.io.IOException;
 import java.util.Scanner;
 
-/* Creating a program that will convert a word document to a pdf document. First attempt at making a program that
-will rely on a third party api, in this case API2Convert which gives 30 free conversions a day.
+/* Creating a program that will convert a jpg to png and vice versa
+    Uses imgBB for photo upload and API2Convert for conversion
+    Uses jackson for JSON parson and okhttp3 for http requests
  */
 
 public class Main {
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    public static String apiKey;
+    protected static String apiKey;
+    protected static String imgBBKey;
     final OkHttpClient client = new OkHttpClient();
 
     String post(String url, String json) throws IOException { //Posting to API what to convert
@@ -39,8 +41,24 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter API key");
+        System.out.println("Enter API2Convert key");
         apiKey = in.nextLine();
+        System.out.println("Enter imgBB key");
+        imgBBKey = in.nextLine();
+
+        System.out.println("Local file (1) or URL (2)?");
+        int choice = in.nextInt();
+        if (choice == 1){ //Can probably be converted to parse string for http to determine
+            System.out.println("File path");
+            String filePath = in.nextLine();
+        }
+        else{
+            System.out.println("URL");
+            String url = in.nextLine();
+        }
+
+
+
 
 
 
