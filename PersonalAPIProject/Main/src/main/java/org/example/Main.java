3b372906a19c2575d5c686fbc1e96dbeb925d6c7
maisma2 <maismail63@gmail.com>
2023-09-20 14:52:06 -0500

    Changing Json viewer

--- PersonalAPIProject/Main/src/main/java/org/example/Main.java
+++ PersonalAPIProject/Main/src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import okhttp3.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
+import com.fasterxml.jackson.*;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -17,7 +15,7 @@ public class Main {
     public static String apiKey;
     final OkHttpClient client = new OkHttpClient();
 
-    String post(String url, String json) throws IOException {
+    String post(String url, String json) throws IOException { //Posting to API what to convert
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
                 .url(url)
@@ -33,54 +31,19 @@ public class Main {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         }
-
     }
 
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
-        JSONObject jsonObject = new JSONObject();
+
         System.out.println("Enter API key");
         apiKey = in.nextLine();
-        System.out.println("Choose from the options below \n " +
-                "1. Convert a word document to a pdf document \n " +
-                "2. Convert a pdf document to a word document \n " +
-                "3. Convert a png image to a jpg img \n " +
-                "4. Convert a jpg image to a png img \n ");
-
-        int choice = in.nextInt();
-        Main maininstance = new Main();
-        switch(choice){
-            case 4:
-                String jsonPayload = "{"
-                        + "\"input\": [{"
-                        + "\"type\": \"remote\","
-                        + "\"source\": \"https://png.pngtree.com/png-clipart/20190515/original/pngtree-instagram-social-media-icon-png-image_3572487.jpg\""
-                        + "}],"
-                        + "\"conversion\": [{"
-                        + "\"category\": \"image\","
-                        + "\"target\": \"jpg\","
-                        + "\"options\": {"
-                        + "\"resize\": \"800x800\","
-                        + "\"quality\": 90"
-                        + "\"target_format\": \"jpg\""
-                        + "}"
-                        + "}]"
-                        + "}";
-            try {
-                String response = maininstance.post("https://api.api2convert.com/v2/jobs", jsonPayload);
-                Thread.sleep(10000);
-                Object obj = JSONValue.parse(response);
-                JSONArray array = (JSONArray)obj;
-            } catch (IOException e) {
-                System.out.println("Error, IOException");
-            }
 
 
-        }
+
     }
 
 
