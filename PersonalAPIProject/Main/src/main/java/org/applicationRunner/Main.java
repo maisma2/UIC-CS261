53f57d45f84551c6e5389b1e3936441ae7b80a4d
maisma2 <maismail63@gmail.com>
2023-10-25 21:47:55 -0500

    Timeline and packages
    Packages need to be optimized

--- PersonalAPIProject/Main/src/main/java/org/applicationRunner/Main.java
+++ PersonalAPIProject/Main/src/main/java/org/applicationRunner/Main.java
@@ -1,6 +1,8 @@
 package org.applicationRunner;
 
 import okhttp3.*;
+import com.fasterxml.jackson.*;
+import com.google.api.*;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,6 +10,7 @@ import java.util.Scanner;
 //Handling HEIC to JPG conversion
 //Uses Google Photo's API and OAuth Clients for image upload for easy batching
 //Uses ConvertAPI to convert the files, using the REST api over SDK because this is a learning project
+//Downloads the ConvertAPI into a destination folder
 
 public class Main {
 
